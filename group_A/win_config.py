aaf2028f2c559bd23dd02983c5233030c049ef48
mjwest99 <143457478+mjwest99@users.noreply.github.com>
2024-10-23 23:07:51 -0700

    Add files via upload

---
+++ group_A/win_config.py
@@ -0,0 +1,5 @@
+# Configuration file for Windows host
+
+# IP address and port for the socket server
+HOST = '0.0.0.0'  # Bind to all interfaces
+PORT = 65432      # Port number for the socket server
