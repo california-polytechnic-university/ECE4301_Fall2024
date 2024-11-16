9213f20c805206aa5f16e6bcb04c0024806fe679
Matthew West <mjwest99.academic@gmail.com>
2024-11-15 17:07:11 -0800

    renamed:    group_A/midterm_rpi.py -> group_A/Matthew West/Midterm/midterm_rpi.py 	renamed:    group_A/midterm_win.py -> group_A/Matthew West/Midterm/midterm_win.py 	renamed:    group_A/requirements.txt -> group_A/Matthew West/Midterm/requirements.txt 	renamed:    group_A/rpi_config.py -> group_A/Matthew West/Midterm/rpi_config.py 	renamed:    group_A/win_config.py -> group_A/Matthew West/Midterm/win_config.py 	new file:   group_A/Matthew West/Quiz 2/Primes_RNG_Demo.mp4

--- group_A/rpi_config.py
+++
@@ -1,5 +0,0 @@
-# Configuration file for Raspberry Pi
-
-# IP address and port for the Windows host
-WINDOWS_HOST_IP = 'WINDOWS_HOST_IP'  # Replace with the actual IP address of the Windows host
-PORT = 65432                         # Port number for the socket server
